aba62c7c00b8e64568c14c6157460fab4f24fb6d
rpnunia <rpnunia@watermarkinsights.com>
2022-09-27 21:44:29 +0530

    v4

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import { fireEvent, render, screen } from "@testing-library/react";
-import App from "./App";
+import App, { replaceCamelWithSpaces } from "./App";
 
 // npm test src/App.test.js ~> to run test file and we need to install Jest "npm install jest"
 
@@ -76,4 +76,18 @@ test("Clicked disabled button has gray background and reverts to blue", () => {
     // re-enable button
     fireEvent.click(checkbox);
     expect(colorButton).toHaveStyle("background-color: blue");
+});
+
+describe("spaces before camel-case capital letters", () => {
+    test("Works for no inner capital letters", () => {
+        expect(replaceCamelWithSpaces("Red")).toBe("Red");
+    });
+
+    test("Works for one inner capital letter", () => {
+        expect(replaceCamelWithSpaces("MidnightBlue")).toBe("Midnight Blue");
+    });
+
+    test("Works for multiple inner capital letters", () => {
+        expect(replaceCamelWithSpaces("MediumVioletRed")).toBe("Medium Violet Red");
+    });
 });
